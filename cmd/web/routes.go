74db8593107cf1ca454ddd0f9c25a44ad5492852
Matthew Downs <mwdowns@gmail.com>
2021-09-06 08:02:05 -0400

    add fileserver to routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,5 +22,7 @@ func routes(a *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileserver := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
 	return mux
 }
